a2823a91dc129dd1a44a4b36d21a758871b8c05f
Misaki Kasumi <misakikasumi@outlook.com>
2023-10-19 20:31:18 +0800

    remove example

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,6 @@
 //! (start with `controller::Controller`), but note that the API of these
 //! internal modules is much more likely to change. Some or all of these
 //! modules might be removed in the future.
-//!
-//! "Hello world" example:
-//! ```
-//! use bat::PrettyPrinter;
-//!
-//! PrettyPrinter::new()
-//!     .input_from_bytes(b"<span style=\"color: #ff00cc\">Hello world!</span>\n")
-//!     .language("html")
-//!     .print()
-//!     .unwrap();
-//! ```
 
 #![deny(unsafe_code)]
 

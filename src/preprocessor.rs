0320a2850d91b0fb179d9447aebd11c9e036514f
Misaki Kasumi <misakikasumi@outlook.com>
2023-10-27 04:54:41 +0800

    fix tabstop

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -51,9 +51,10 @@ pub(crate) fn replace_nonprintable(
     tab_width: usize,
     nonprintable_notation: NonprintableNotation,
 ) -> String {
-    let mut output = String::new();
+    let mut output = Vec::with_capacity(input.len());
     let tab_width = if tab_width == 0 { 4 } else { tab_width };
     let mut line_idx = 0;
+    let mut buf = String::with_capacity(6);
     for chunk in input.utf8_chunks() {
         for chr in chunk.valid().chars() {
             let mut before_size = output.len();
@@ -73,9 +74,9 @@ pub(crate) fn replace_nonprintable(
                 }
                 // line feed
                 '\x0A' => {
-                    output.push_str(match nonprintable_notation {
-                        NonprintableNotation::Caret => "^J\x0A",
-                        NonprintableNotation::Unicode => "␊\x0A",
+                    output.extend_from_slice(match nonprintable_notation {
+                        NonprintableNotation::Caret => &['^', 'J', '\x0A'],
+                        NonprintableNotation::Unicode => &['␊', '\x0A'],
                     });
                     before_size = output.len();
                 }
@@ -98,7 +99,7 @@ pub(crate) fn replace_nonprintable(
                 }
                 // delete
                 '\x7F' => match nonprintable_notation {
-                    NonprintableNotation::Caret => output.push_str("^?"),
+                    NonprintableNotation::Caret => output.extend_from_slice(&['^', '?']),
                     NonprintableNotation::Unicode => output.push('\u{2421}'),
                 },
                 // printable ASCII
@@ -114,10 +115,12 @@ pub(crate) fn replace_nonprintable(
             line_idx += output.len() - before_size;
         }
         for byte in chunk.invalid() {
-            write!(output, "\\x{:02X}", byte).unwrap();
+            write!(buf, "\\x{:02X}", byte).unwrap();
+            output.extend(buf.chars());
+            buf.clear();
             line_idx += 6;
         }
     }
 
-    output.into()
+    output.into_iter().collect()
 }
